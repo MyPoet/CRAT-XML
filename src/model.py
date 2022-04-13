d6759eec0ddb08571575840a4ee77bcbd8082f5a
MyPoet <282529161@qq.com>
2022-04-13 14:30:43 +0000

    add model file

---
+++ src/model.py
@@ -0,0 +1,31 @@
+import time
+
+import tqdm
+import numpy as np
+from apex import amp
+from transformers import BertTokenizer, BertConfig, BertModel
+from transformers import RobertaModel, RobertaConfig, RobertaTokenizer
+from transformers import XLNetTokenizer, XLNetModel, XLNetConfig
+
+def get_bert(bert_name):
+    if 'roberta' in bert_name:
+        print('load roberta-base')
+        model_config = RobertaConfig.from_pretrained('roberta-base')
+        model_config.output_hidden_states = True
+        bert = RobertaModel.from_pretrained('roberta-base', config=model_config)
+    elif 'xlnet' in bert_name:
+        print('load xlnet-base-cased')
+        model_config = XLNetConfig.from_pretrained('xlnet-base-cased')
+        model_config.output_hidden_states = True
+        bert = XLNetModel.from_pretrained('xlnet-base-cased', config=model_config)
+    else:
+        print('load bert-base-uncased')
+        model_config = BertConfig.from_pretrained('bert-base-uncased')
+        model_config.output_hidden_states = True
+        bert = BertModel.from_pretrained('bert-base-uncased', config=model_config)
+    return bert
+
+class CaRtXML(nn.Module):
+    def __init__(self, num_labels, bert='bert-base', dropout=0.5,):
+        super(CaRtXML, self).__init__()
+
